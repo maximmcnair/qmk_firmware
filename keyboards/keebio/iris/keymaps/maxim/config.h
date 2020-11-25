3493c2adfb28f1fbc9fbc190ac6bec432c9a3a00
Maxim McNair <maxim@neverbland.com>
2020-11-25 09:58:50 +0000

    commit code left from ages ago

--- keyboards/keebio/iris/keymaps/maxim/config.h
+++ keyboards/keebio/iris/keymaps/maxim/config.h
@@ -34,3 +34,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
+
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 20
